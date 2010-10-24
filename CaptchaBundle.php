400bbb9e56f2ed0aa735ad94b51824fdb4c5025d
Sergei K <zakkord@gmail.com>
2010-10-24 08:21:21 +0700

    initial commit

---
+++ CaptchaBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bundle\CaptchaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class CaptchaBundle extends Bundle
+{
+}
